384f52411d91db3401b448135d2d11f094dbd47b
Tomasz <nowakowski.tomasz.j@gmail.com>
2020-05-25 20:04:40 +0200

    adding new placeholder data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'projekt';
+  pi = Math.PI;
+  date = new Date();
+  dog: string;
+
+
+  showDog() {
+    const dog = new Dog('Andrzej', 2);
+    return 'My dog is ' + dog.name + 'and He is' + dog.age;
+  }
+
+
+}
+
+class Dog {
+  constructor(public name: string, public age: number) { }
 }
