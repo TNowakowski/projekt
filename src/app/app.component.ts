e1f67ed7e5c29c31091f7fcaca09075f512ebd38
Tomasz <nowakowski.tomasz.j@gmail.com>
2020-05-26 17:15:33 +0200

    more changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,8 +11,22 @@ export class AppComponent {
   date = new Date();
   dog: string;
   dogs = new Array<Dog>();
+  days = ['poniedziałek', 'wtorek', 'środa', 'czwartek', 'piątek', 'sobota', 'niedziela'];
+
+  inputText = 'Tekst';
+  maxLength = 6;
+  colorClass = 'color';
+  logoUrl = '';
+
+  isDisabled = true;
+
 
   constructor() {
+
+    setTimeout(() => {
+      this.isDisabled = false;
+    }, 3000);
+
     this.dogs.push(new Dog('szmak', 2), new Dog('killwater', 6), new Dog('ies', 200));
   }
 
@@ -21,6 +35,12 @@ export class AppComponent {
     return 'My dog is ' + dog.name + 'and He is' + dog.age;
   }
 
+  change() {
+    this.inputText = 'Zmiana tekstu i koloru';
+    this.maxLength = 10;
+    this.colorClass = 'color2';
+    this.logoUrl = 'https://angular.io/assets/images/logos/angular/angular.png';
+  }
 
 }
 
