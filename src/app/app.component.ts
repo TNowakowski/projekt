fe72e352ee1598e6c31aaa3a9d370935966dde5d
Tomasz <nowakowski.tomasz.j@gmail.com>
2020-05-26 19:52:32 +0200

    task list excercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  task: string;
+  tasksToDo = new Array<string>();
+  tasksDone = new Array<string>();
+
+  addTask() {
+    if (this.task !== undefined && this.task.trim() !== '') {
+      this.tasksToDo.push(this.task);
+    }
+    this.task = '';
+  }
+
+  remove(task: string) {
+    this.tasksToDo = this.tasksToDo.filter(e => e !== task);
+  }
+
+  done(task: string) {
+    this.tasksDone.push(task);
+    this.remove(task);
+  }
 }
