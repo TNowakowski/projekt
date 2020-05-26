3717fff8596bd6032b61fcfcd6d17eb06a503153
Tomasz <nowakowski.tomasz.j@gmail.com>
2020-05-26 18:24:38 +0200

    removing old testing data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,44 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'projekt';
-  pi = Math.PI;
-  date = new Date();
-  dog: string;
-  dogs = new Array<Dog>();
-  days = ['poniedziałek', 'wtorek', 'środa', 'czwartek', 'piątek', 'sobota', 'niedziela'];
 
-  inputText = 'Tekst';
-  maxLength = 6;
-  colorClass = 'color';
-  logoUrl = '';
-
-  isDisabled = true;
-
-
-  constructor() {
-
-    setTimeout(() => {
-      this.isDisabled = false;
-    }, 3000);
-
-    this.dogs.push(new Dog('szmak', 2), new Dog('killwater', 6), new Dog('ies', 200));
-  }
-
-  showDog() {
-    const dog = new Dog('Andrzej', 2);
-    return 'My dog is ' + dog.name + 'and He is' + dog.age;
-  }
-
-  change() {
-    this.inputText = 'Zmiana tekstu i koloru';
-    this.maxLength = 10;
-    this.colorClass = 'color2';
-    this.logoUrl = 'https://angular.io/assets/images/logos/angular/angular.png';
-  }
-
-}
-
-class Dog {
-  constructor(public name: string, public age: number) { }
 }
